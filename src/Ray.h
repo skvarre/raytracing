3f614cc182d2de9042e31d06f5d533717e3d2169
timolsen <timolsen@kth.se>
2022-03-16 10:05:18 +0100

    added arithmetic operators for Vec

--- src/Ray.h
+++ src/Ray.h
@@ -1,12 +1,20 @@
 #ifndef RAY_H
 #define RAY_H
-
+/**
+ * Class describing a ray
+ */
 #include "Vec.h"
 
 class Ray {
 public:
-private:
+    Ray() : m_A(Vec()), m_B(Vec()) {}
+    Ray(const Vec & A, const Vec & B) : m_A(A), m_B(B) {}
 
-}
+    Vec P(float t) { return m_A + m_B*t; }
+
+private:
+    Vec m_A;
+    Vec m_B;
+};
 
 #endif
--- src/Vec.h
+++ src/Vec.h
@@ -1,5 +1,6 @@
 #ifndef VEC_H
 #define VEC_H
+
 /**
  * Class describing a 3-dimensional vector 
  */
@@ -8,13 +9,28 @@ public:
     Vec() : m_x(0), m_y(0), m_z(0) {}
     Vec(float x, float y, float z) : m_x(x), m_y(y), m_z(z) {}
 
+    //Accessors
     float x() const { return m_x; }
     float y() const { return m_y; }
     float z() const { return m_z; }
+
+    void x(float x) { m_x = x; }
+    void y(float y) { m_y = y; }
+    void z(float z) { m_z = z; }
+
+
 private:
     float m_x;
     float m_y;
     float m_z;
+};
+
+Vec operator-(const Vec &lhs, const Vec &rhs){
+    return Vec(lhs.x() - rhs.x(), lhs.y() - rhs.y(), lhs.z() - rhs.z());
+}
+
+Vec operator+(const Vec &lhs, const Vec &rhs) {
+    return Vec(lhs.x() + rhs.x(), lhs.y() + rhs.y(), lhs.z() + rhs.z());
 }
 
 #endif
