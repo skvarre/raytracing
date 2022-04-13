81da5dc56d428cbb4a248b3417328e4fe3241220
timolsen <timolsen@kth.se>
2022-04-13 16:48:03 +0200

    ja det funkar ju

--- src/GPU/Sphere.h
+++ src/GPU/Sphere.h
@@ -7,7 +7,7 @@
 class Sphere {
 public:
     __host__ __device__ Sphere() : m_c(Vec()), m_r(0) {}
-    __host__ __device__  Sphere(const Vec & c, const float r) : m_c(c), m_r(r) {}
+    __host__ __device__ Sphere(const Vec & c, const float r) : m_c(c), m_r(r) {}
 
     __host__ __device__ Vec c() const { return m_c; }
     __host__ __device__ float r() const { return m_r; }
